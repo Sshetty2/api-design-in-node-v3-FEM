91c528ac882cde4c4f351a5ab79de9f15717ed90
Sachit Shetty <sachit.shetty@gmail.com>
2019-02-13 17:19:07 -0500

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
-import config from './config'
 import cors from 'cors'
-import { connect } from './utils/db'
 
 export const app = express()
 
@@ -14,13 +12,4 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = async () => {
-  try {
-    await connect()
-    app.listen(config.port, () => {
-      console.log(`REST API on http://localhost:${config.port}/api`)
-    })
-  } catch (e) {
-    console.error(e)
-  }
-}
+export const start = () => {}
